7611c66d7982aee285ade27d6e7a756e1f1b7191
Stefan Lekrem <lekrem@icloud.com>
2024-09-22 11:11:20 +0200

    Update app-shell.js

--- src/js/app-shell.js
+++ src/js/app-shell.js
@@ -1,10 +1,8 @@
 import { html, LitElement } from 'lit';
 
 export default class AppShell extends LitElement {
-    static get is() { return 'app-shell'; }
-
     createRenderRoot() { return this; }
-    render() {  return html`asd`;  }
+    render = () => { return html`asd`; }
 }
 
-customElements.define(AppShell.is, AppShell);
+customElements.define('app-shell', AppShell);
